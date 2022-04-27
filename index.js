fb11c5a73edc4a9e05fb811d43a1be88a0bc29d2
Travis Fehr <travisefehr@gmail.com>
2022-04-27 09:22:24 -0700

    commit tree 23f2873d43db41ce0ac1e785604581584ddd736f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+  parseInt(pickUp);
+  return Math.abs(pickUp -42);
+}
+
+function distanceFromHqInFeet(pickUp) {
+  return (distanceFromHqInBlocks(pickUp) * 264);
+}
+
+function distanceTravelledInFeet(startB, destinationB) {
+  return Math.abs((destinationB - startB) * 264);
+}
+
+function calculatesFarePrice(startB, destinationB) {
+  let feetTravelled = distanceTravelledInFeet(startB, destinationB);
+  if (feetTravelled <= 400) {
+    return 0;
+  }
+  else if (feetTravelled > 400 && feetTravelled <= 2000) {
+    return ((feetTravelled - 400) * .02);
+  }
+  else if (feetTravelled > 2000 && feetTravelled <= 2500) {
+    return 25;
+  }
+  else if (feetTravelled > 2500) {
+    return 'cannot travel that far';
+  }
+}
